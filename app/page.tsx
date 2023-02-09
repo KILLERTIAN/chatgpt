9910e694adfde544d06059d94f2950f6f3262078
Om Sharma <omsharma050322@gmail.com>
2023-02-09 23:20:08 +0530

    Minor UI Changes in Basic Chat page build

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ function HomePage() {
     return (
         <div className="flex flex-col items-center justify-center h-screen px-2 text-white">
             <h1 className="text-5xl font-bold mb-20">ChatGPT</h1>
-            <div>
+            <div className='flex space-x-2 text-center'>
                 <div>
                     <div className="flex flex-col items-center justify-center mb-5">
                         {/* Sun Icon */}
