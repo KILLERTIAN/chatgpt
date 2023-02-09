2ac24aaf38e4d5d06ca006c863dc30cb42bb5b72
Om Sharma <omsharma050322@gmail.com>
2023-02-09 23:14:39 +0530

    Basic ChatIntro layout-1

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { SunIcon } from '@heroicons/react/24/outline'
+import { BoltIcon, ExclamationTriangleIcon, SunIcon } from '@heroicons/react/24/outline'
 
 function HomePage() {
     return (
@@ -15,6 +15,30 @@ function HomePage() {
                         <p className="infoText">"Explain something to me"</p>
                         <p className="infoText">"What is diffrence between dog and a cat"</p>
                         <p className="infoText">"What is the color of the sun"</p>
+                    </div>
+                </div>
+                <div>
+                    <div className="flex flex-col items-center justify-center mb-5">
+                        {/* Sun Icon */}
+                        <BoltIcon className="h-8 w-8" />
+                        <h2>Capabilities</h2>
+                    </div>
+                    <div className="space-y-2">
+                        <p className="infoText">Change the ChatGPT model to use </p>
+                        <p className="infoText">Messages are stored of firestore's firebase</p>
+                        <p className="infoText">hot toast notification when ChatGPT is thinking!</p>
+                    </div>
+                </div>
+                <div>
+                    <div className="flex flex-col items-center justify-center mb-5">
+                        {/* Sun Icon */}
+                        <ExclamationTriangleIcon className="h-8 w-8" />
+                        <h2>Limitation</h2>
+                    </div>
+                    <div className="space-y-2">
+                        <p className="infoText">May ocassionly generate incorrect information</p>
+                        <p className="infoText">May ocassionly produce harmful instructions of biased content</p>
+                        <p className="infoText">Limited knowledge of world and events after 2021</p>
                     </div>
                 </div>
             </div>
