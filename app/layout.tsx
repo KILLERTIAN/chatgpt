382b71b2162066cf425c59b21e489fb664fb05ba
Om Sharma <omsharma050322@gmail.com>
2023-02-12 22:01:32 +0530

    New Chat Button

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,10 @@ export default function RootLayout({
       <head />
       <body>
         <div className='flex'>
-          {/* SideBar */}
-          <SideBar />
+          <div className='bg-[#202123] max-w-xs h-screen overflow-y-auto md:min-w-[20rem]'>
+            <SideBar />
+          </div>
+
           {/* ClientProvider - Notification */}
           <div className="bg-[#343541] flex-1">{children}</div>
         </div>
--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,11 +1,11 @@
-
+import NewChat from "./NewChat"
 function SideBar() {
     return (
         <div className="p-2 flex flex-col h-screen">
-            SideBar Goes Here
             <div className="flex-1">
                 <div>
                     {/* NewChat */}
+                    <NewChat />
                     <div>
                         {/* ModelSelection */}
                     </div>
