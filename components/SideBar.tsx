9c28b2a9f9beeaee731b1b4e25840835e7e6c1b5
Om Sharma <omsharma050322@gmail.com>
2023-02-17 23:44:47 +0530

    Added Google Auth and Added Logout Func

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,5 +1,8 @@
+'use client'
+import { signOut, useSession } from "next-auth/react";
 import NewChat from "./NewChat"
 function SideBar() {
+    const { data: session } = useSession();
     return (
         <div className="p-2 flex flex-col h-screen">
             <div className="flex-1">
@@ -12,7 +15,9 @@ function SideBar() {
                     {/* Map through the ChatRows */}
                 </div>
             </div>
-
+            {session &&
+                <img onClick={() => signOut()} src={session.user?.image!} alt="Profile pic" className="h-12 w-12 rounded-full cursor-pointer mx-auto mb-2 hover:opacity-50" />
+            }
         </div>
     )
 }
