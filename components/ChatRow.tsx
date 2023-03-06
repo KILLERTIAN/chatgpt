799e7fae976e8640b3082508934696ea431a82e1
Om Sharma <omsharma050322@gmail.com>
2023-03-06 23:05:04 +0530

    ActiveChat background

--- components/ChatRow.tsx
+++ components/ChatRow.tsx
@@ -4,8 +4,8 @@ import { collection, orderBy, query } from "firebase/firestore";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { useRouter } from "next/router";
-import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 import { useCollection } from "react-firebase-hooks/firestore";
 import { db } from "../firebase";
 
@@ -14,13 +14,18 @@ type Props = {
 };
 
 function ChatRow({ id }: Props) {
-    const pathName = usePathname();
+    const pathname = usePathname();
     const router = useRouter();
     const { data: session } = useSession();
     const [active, setActive] = useState(false);
-    const [messages] = useCollection(query(collection(db, 'users', session?.user?.email!, 'chats', id, 'messages'), orderBy('createdAt', 'asc')))
+    const [messages] = useCollection(query(collection(db, 'users', session?.user?.email!, 'chats', id, 'messages'), orderBy('createdAt', 'asc')));
+
+    useEffect(() => {
+        if (!pathname) return;
+        setActive(pathname.includes(id));
+    }, [pathname])
     return (
-        <Link href={`/chat/${id}`} className={`chatRow justify-center`}>
+        <Link href={`/chat/${id}`} className={`chatRow justify-center ${active && 'bg-gray-700/50'}`}>
             <ChatBubbleLeftIcon className="h-5 w-5" />
             <p className="flex-1 hidden md:inline-flex truncate">
                 {messages?.docs[messages?.docs.length - 1]?.data().text || "New Chat"}
