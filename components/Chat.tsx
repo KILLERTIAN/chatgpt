3b7e770f842561c2083b0bc7fa32b6b1180ae5f0
Om Sharma <omsharma050322@gmail.com>
2023-03-21 22:33:19 +0530

    updated Chat

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -1,9 +1,24 @@
+"use client";
+
+import { collection, orderBy, query } from "firebase/firestore";
+import { useSession } from "next-auth/react";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { db } from "../firebase";
+
 type Props = {
     chatId: string;
 }
 function Chat({ chatId }: Props) {
+    const { data: session } = useSession();
+
+    const [messages] = useCollection(session && query(collection(db, "users", session?.user?.email!, "chats", chatId, "messages"),
+        orderBy("createdAt", "asc")
+    ))
+
     return (
-        <div className="flex-1">Chat</div>
+        <div className="flex-1">
+            { }
+        </div>
     )
 }
 
