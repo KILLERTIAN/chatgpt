23eddcad4c838cd566f8e771567fa42fe9f8013d
Om Sharma <omsharma050322@gmail.com>
2023-03-23 22:43:35 +0530

    Fixed a minor issue

--- components/Message.tsx
+++ components/Message.tsx
@@ -8,10 +8,12 @@ function Message({ message }: Props) {
     const isChatGPT = message.user.name === "ChatGPT";
     return (
         <div className={`py-5 text-white ${isChatGPT && "bg-[#434654]"}`}>
-            <img src={message.user.avatar} alt="" className="h-8 w-8" />
-            <p className="pt-1 text-sm ">
-                {message.txt}
-            </p>
+            <div >
+                <img src={message.user.avatar} alt="" className="h-8 w-8" />
+                <p className="pt-1 text-sm ">
+                    {message.text}
+                </p>
+            </div>
         </div >
     )
 }
