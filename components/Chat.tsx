07c4906f81a69a08cffe40122845632b80a2aff6
Om Sharma <omsharma050322@gmail.com>
2023-05-23 23:13:06 +0530

    flex updates

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -17,7 +17,7 @@ function Chat({ chatId }: Props) {
     ))
 
     return (
-        <div className="flex-1">
+        <div className="flex-1 overflow-y-auto overflow-x-hidden">
             {messages?.docs.map((message) => (
                 <Message key={message.id} message={message.data()} />
             ))}
